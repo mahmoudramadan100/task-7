0c46dd61503357eab0dfe05dc8e0f7d8d01eb52e
mahmoud ramadan <mahmoudr03@gmail.com>
2022-12-20 15:55:35 +0200

    Start Project

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 // let section = document.querySelector('.section-3');
 // let span = document.querySelector('.section-3 span');
-let btn = document.querySelector('.btn');
+let btn = document.querySelector('.scroll-top');
+let navbar = document.querySelector('.navbar');
 
 window.onscroll = function() {
-    // if (window.scrollY >= section.offsetTop)  {
-    //     span.classList.add('color');
-    //     console.log("hello")
-    // }
+    if (window.scrollY >= 100)  {
+        navbar.classList.add('navbar-scroll');
+        navbar.style.transition = "all 0.5s";
+    } else {
+        // navbar.style.top = "45px";
+        navbar.classList.remove('navbar-scroll');
+    }
     if(window.scrollY >= 700) {
         btn.style.display= "block"
     } else {
@@ -14,9 +18,9 @@ window.onscroll = function() {
     }
 };
 
-window.onclick = function() {
-    scroll({
-        top: 0,
-        behavior: 'smooth'
-    });
-}
+// window.onclick = function() {
+//     scroll({
+//         top: 0,
+//         behavior: 'smooth'
+//     });
+// }
